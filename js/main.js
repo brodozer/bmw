c464b133505196010dbaaeb0b4f9ad59e92ed7d2
Aleksandr Perepelitsa <perepelitsa.alex@gmail.com>
2021-05-20 17:53:47 +0300

    lesson_3

--- js/main.js
+++ js/main.js
@@ -29,15 +29,17 @@ const accordeon = () => {
     });
 };
 
-const smoothScroll = () => {
-    const link = document.querySelectorAll('[href^="#"]:not([href="#"])');
+const smoothScroll = (menu) => {
+    const link = document.querySelectorAll('.menu-list__link, .main__scroll, .main__button');
 
     link.forEach((l) => {
         l.addEventListener('click', (e) => {
-            e.preventDefault();
-            const id = l.getAttribute('href').substring(1);
-            //console.log('id:', id);
-            document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+            const id = l.getAttribute('href');
+            if (id.indexOf('#') !== -1) {
+                e.preventDefault();
+                document.getElementById(id.substring(1)).scrollIntoView({ behavior: 'smooth' });
+            }
+            menu.closeMenu();
         });
     });
 };
@@ -66,6 +68,53 @@ const tabs = () => {
     }
 };
 
+const modal = () => {
+    const popup = document.querySelector('.modal');
+    const popupBtns = document.querySelectorAll('.more');
+
+    const popupOpen = () => {
+        popup.classList.remove('hidden');
+    };
+
+    const popupClose = () => {
+        popup.classList.add('hidden');
+    };
+
+    popupBtns.forEach((btn) => {
+        btn.addEventListener('click', popupOpen);
+    });
+
+    popup.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains('overlay') || target.classList.contains('modal__close')) {
+            popupClose();
+        }
+    });
+};
+
+class Menu {
+    constructor() {
+        this.btn = document.querySelector('.humburger-menu');
+        this.nav = document.querySelector('.menu');
+    }
+
+    closeMenu() {
+        if (this.nav.classList.contains('menu-active')) {
+            this.nav.classList.remove('menu-active');
+        }
+    }
+
+    listener() {
+        this.btn.addEventListener('click', () => {
+            this.nav.classList.toggle('menu-active');
+        });
+    }
+}
+
+const menu = new Menu();
+
+menu.listener();
+smoothScroll(menu);
 accordeon();
-smoothScroll();
 tabs();
+modal();
